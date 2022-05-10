b218fa324f579486bc33c84aa82381bb3b9d0a53
MostafaKadyy <kady201074@gamil.com>
2022-05-10 07:01:30 +0200

    add chrome headless

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,77 +23,81 @@ import org.testng.annotations.Parameters;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import utilities.Helper;
 
-public class TestBase extends AbstractTestNGCucumberTests
-{
-	public static WebDriver driver ;
-	public static String downloadPath = System.getProperty("user.dir")+"/downloads";
-	
-	public static FirefoxOptions firefoxoption() 
-	{
+public class TestBase extends AbstractTestNGCucumberTests {
+	public static WebDriver driver;
+	public static String downloadPath = System.getProperty("user.dir") + "/downloads";
+
+	public static FirefoxOptions firefoxoption() {
 		FirefoxOptions option = new FirefoxOptions();
-		option.addPreference("browser.download.folderlist",2);
-		option.addPreference("browser.download.dir",downloadPath);
-		option.addPreference("browser.helperApps.neverAsk.saveToDisk","application/pdf,application/octet-stream");
-		option.addPreference("browser.download.manager.showWhenStarting",false);
-		option.addPreference("pdfjs.disabled",true);
-		return option;		
+		option.addPreference("browser.download.folderlist", 2);
+		option.addPreference("browser.download.dir", downloadPath);
+		option.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf,application/octet-stream");
+		option.addPreference("browser.download.manager.showWhenStarting", false);
+		option.addPreference("pdfjs.disabled", true);
+		return option;
 	}
-	
-	public static ChromeOptions chromeOption() 
-	{
+
+	public static ChromeOptions chromeOption() {
 		ChromeOptions options = new ChromeOptions();
 		HashMap<String, Object> chromeprefs = new HashMap<String, Object>();
-		chromeprefs.put("profile.defult.content_setting.popup",0);
-		chromeprefs.put("profile.defult.directory",downloadPath);
+		chromeprefs.put("profile.defult.content_setting.popup", 0);
+		chromeprefs.put("profile.defult.directory", downloadPath);
 		options.setExperimentalOption("prefs", chromeprefs);
-		options.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
-		return options;		
+		options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		return options;
 	}
-	
+
 	@BeforeSuite
-	@Parameters({"browser"})
-	public void startDriver ( @Optional("headless") String browserName) 
-	{
+	@Parameters({ "browser" })
+	public void startDriver(@Optional("chrome-headless") String browserName) {
 		if (browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
-			driver =new ChromeDriver(chromeOption());
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+			driver = new ChromeDriver(chromeOption());
 		}
-		
-		else if (browserName.equalsIgnoreCase("firefox")) 
-		{
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\drivers\\geckodriver.exe");
-			driver =new FirefoxDriver(firefoxoption());
+
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver(firefoxoption());
 		}
-		else if(browserName.equalsIgnoreCase("ie")) 
-		{
-			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\drivers\\IEDriverServer.exe");
-			driver =new InternetExplorerDriver();
+
+		else if (browserName.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\drivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 		}
-		else if (browserName.equalsIgnoreCase("headless"))
-		{
+
+		else if (browserName.equalsIgnoreCase("headless")) {
 			DesiredCapabilities caps = new DesiredCapabilities();
 			caps.setJavascriptEnabled(true);
 			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-				"E:\\mostafa\\.1testing\\selenium-framework\\drivers\\phantomjs.exe");
-			String[] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+					"E:\\mostafa\\.1testing\\selenium-framework\\drivers\\phantomjs.exe");
+			String[] phantomJsArgs = { "--web-security=no", "--ignore-ssl-errors=yes" };
 			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
 			driver = new PhantomJSDriver(caps);
 		}
-		
+
+		else if (browserName.equalsIgnoreCase("chrome-headless")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("--headless");
+			option.addArguments("--window-size=1920,1080");
+			driver = new ChromeDriver(option);
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.navigate().to("https://demo.nopcommerce.com/");
 	}
-	
+
 	@AfterSuite
-	public void stopDriver()
-	{
+	public void stopDriver() {
 		driver.quit();
 	}
+
 	@AfterMethod
-	public void screenshotonFailure(ITestResult result) 
-	{
-		if (result.getStatus()==ITestResult.FAILURE) {
+	public void screenshotonFailure(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
 			System.out.println("falid");
 			System.out.println("taking screenshot...");
 			Helper.captureScreenshot(driver, result.getName());
